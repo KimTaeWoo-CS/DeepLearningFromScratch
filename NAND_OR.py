3dd62921f9d565fb1e79d98f31d14232a2efc58f
KimTaeWoo-CS <cow9404@naver.com>
2021-03-24 15:34:05 +0900

    test_OR

--- NAND_OR.py
+++ NAND_OR.py
@@ -12,10 +12,15 @@ def NAND(x1, x2):
 
 def OR(x1, x2):
     x = np.array([x1, x2])
-    w = np.array([0.5,0.5])
-    b = -0.2
+    w = np.array([1.0,1.0])
+    b = -0.5
     tmp = np.sum(w*x)+b
     if tmp <= 0:
         return 0
     else:
         return 1
+
+print (OR(0,0))
+print (OR(1,0))
+print (OR(0,1))
+print (OR(1,1))
