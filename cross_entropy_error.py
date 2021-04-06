34179c45046117078797c3ac571a23dc80e7de00
KimTaeWoo-CS <cow9404@naver.com>
2021-04-06 17:33:44 +0900

    comment_cross_entropy_error

--- cross_entropy_error.py
+++ cross_entropy_error.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
+    delta = 1e-7 # np.log 함수에 0을 입력하면 마아니스 무한대를 뜻하기 때문에 아주 작은 값을 더한다.
     return -np.sum(t * np.log(y + delta))
 
