416c600523a40de0e70d04ecf5d3bee7482c6d4b
KimTaeWoo-CS <cow9404@naver.com>
2021-04-07 12:45:44 +0900

    cross_entropy_error_for_batch

--- cross_entropy_error.py
+++ cross_entropy_error.py
@@ -1,6 +1,21 @@
 import numpy as np
 
-def cross_entropy_error(y, t):
+'''def cross_entropy_error(y, t):
     delta = 1e-7 # np.log 함수에 0을 입력하면 마아니스 무한대를 뜻하기 때문에 아주 작은 값을 더한다.
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(t * np.log(y + delta))'''
 
+def cross_entropy_error(y,t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y+1e-7)) / batch_size
+
+def cross_entropy_error_not_onehot(y,t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange[batch_size], t] + 1e-7)) / batch_size
