753a2e030aec2c4c14284e2386dc8d3d800c83fe
KimTaeWoo-CS <cow9404@naver.com>
2021-04-08 18:38:39 +0900

    numerical_diff

--- numerical_diff.py
+++ numerical_diff.py
@@ -2,4 +2,8 @@
 #나쁜 구현의 예
 def bad_numerical_diff(f, x):
     h = 10e-50
-    return ( f(x+h) - f(x)) / h
+    return (f(x+h) - f(x)) / h
+
+def numerical_diff(f, x):
+    h = 12-4
+    return (f(x+h) - f(x-h)) / (2*h)
