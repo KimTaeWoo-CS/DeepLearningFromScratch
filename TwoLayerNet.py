e8617afa4309a0339957d57c93f30a1ad1a78fe9
KimTaeWoo-CS <cow9404@naver.com>
2021-06-14 15:29:01 +0900

    mini_batch

--- TwoLayerNet.py
+++ TwoLayerNet.py
@@ -2,6 +2,7 @@ import sys, os
 from functions import *
 from numerical_gradient import numerical_gradient
 from cross_entropy_error import cross_entropy_error
+from sigmoid import sigmoid
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
@@ -29,7 +30,7 @@ class TwoLayerNet:
         return cross_entropy_error(y,t)
 
     def accuracy(self, x, t):
-        y =self.predict(x)
+        y = self.predict(x)
         y = np.argmax(y, axis=1)
         t = np.argmax(t, axis=1)
 
@@ -44,6 +45,31 @@ class TwoLayerNet:
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid(a1) * da1
+        grads['W1'] = np.dot(x.T, dz1)
+        grads['b1'] = np.sum(dz1, axis=0)
 
         return grads
 
@@ -63,5 +89,3 @@ grads['W1'].shape
 grads['b1'].shape
 grads['W2'].shape
 grads['b2'].shape
-
-
