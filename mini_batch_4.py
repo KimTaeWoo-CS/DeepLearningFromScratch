090639fe6959f4250b4de18ce6cc3b9940c26024
KimTaeWoo-CS <cow9404@naver.com>
2021-06-14 15:50:47 +0900

    train_neuralnet

--- mini_batch_4.py
+++ mini_batch_4.py
@@ -1,17 +1,21 @@
 import numpy as np
 from dataset.mnist import load_mnist
 from TwoLayerNet import TwoLayerNet
-import matplotlib.pyplot as plt
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
 insert_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learning_rate = 0.1
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
 
 for i in range(insert_num):
     batch_mask = np.random.choice(train_size, batch_size)
@@ -26,5 +30,9 @@ for i in range(insert_num):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
 
-
-  
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc |" + str(train_acc) + ", " + str(test_acc))
