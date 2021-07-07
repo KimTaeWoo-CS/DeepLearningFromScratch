404dde6116fded12b19088f84e9dab278b63f8b3
KimTaeWoo-CS <cow9404@naver.com>
2021-07-07 11:45:48 +0900

    validation_data

---
+++ ch06/validation_data.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path.append(os.pardir) 
+from dataset.mnist import load_mnist
+import numpy as np
+
+(x_train, t_train), (x_test, t_test) = load_mnist()
+
+#-----------------------------------------------------------------------
+def shuffle_dataset(x, t):
+    """데이터셋을 뒤섞는다.
+
+    Parameters
+    ----------
+    x : 훈련 데이터
+    t : 정답 레이블
+    
+    Returns
+    -------
+    x, t : 뒤섞은 훈련 데이터와 정답 레이블
+    """
+    permutation = np.random.permutation(x.shape[0])
+    x = x[permutation,:] if x.ndim == 2 else x[permutation,:,:,:]
+    t = t[permutation]
+
+    return x, t
+#-----------------------------------------------------------------------
+
+x_train, t_train = shuffle_dataset(x_train, t_train)
+
+validation_rate = 0.20
+validation_num = int(x_train.shape[0] * validation_rate)
+
+x_val = x_train[:validation_num]
+t_val = t_train[:validation_num]
+x_train = x_train[validation_num:]
+t_train = t_train[validation_num:]
