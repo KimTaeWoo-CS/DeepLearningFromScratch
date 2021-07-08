e38d74e8289cbb6d23161680073177565f188d4e
KimTaeWoo-CS <cow9404@naver.com>
2021-07-08 16:11:24 +0900

    Convolution

--- ch07/im2col.py
+++ ch07/im2col.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.lib.arraypad import pad
+
 
 #-----------------------------------------------------------------------
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
@@ -34,10 +34,22 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     return col
 #-----------------------------------------------------------------------
 
-x1 = np.random.rand(1,3,7,7)
-col1 = im2col(x1, 5, 5, stride=1, pad=0)
-print(col1.shape)
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
 
-x2 = np.random.rand(10,3,7,7)
-col2 = im2col(x2, 5, 5, stride=1, pad=0)
-print(col2.shape)
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0,3,1,2)
+
+        return out
